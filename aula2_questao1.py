eea3462af67b158551121d852ff0882bca5b3b36
GabrielReis416 <166262720+GabrielReis416@users.noreply.github.com>
2024-06-08 23:46:07 -0300

    Delete aula2_questao1.py

--- aula2_questao1.py
+++
@@ -1,9 +0,0 @@
-idade_juliana = int(input("idade de juliana :"))
-idade_cris= int(input("idade de cris :"))
-   
-if idade_juliana >=18 and idade_cris >=18:
-    print("pode entrar")
-else:
-    print("nao pode")
-
-                                                                       
