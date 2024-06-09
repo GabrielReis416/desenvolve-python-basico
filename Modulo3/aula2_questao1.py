6f7b0c7b943797f9ddc671f03987bba572b9a406
GabrielReis416 <166262720+GabrielReis416@users.noreply.github.com>
2024-06-08 23:46:39 -0300

    Add files via upload

---
+++ Modulo3/aula2_questao1.py
@@ -0,0 +1,9 @@
+idade_juliana = int(input("idade de juliana :"))
+idade_cris= int(input("idade de cris :"))
+   
+if idade_juliana >=18 and idade_cris >=18:
+    print("pode entrar")
+else:
+    print("nao pode")
+
+                                                                       
